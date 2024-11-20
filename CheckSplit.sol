4e06a80e577f39e5f2d8c6b492a17f18e6001c3e
Shayaan2972 <112866443+Shayaan2972@users.noreply.github.com>
2024-11-19 21:47:08 -0500

    Add files via upload

---
+++ CheckSplit.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title Check Splitting Interface and Contract
+/// @Splits a bill among participants
+interface ICheckSpiltter{
+
+    /// @dev Emitted when the bill is split among participants.
+    /// @param totalAmount The total Ether amount to be split.
+    /// @param numParticipants The number of participants involved.
+    /// @param perParticipant The amount allocated to each participant.
+    event EtherSplit(
+        uint256 totalAmount,
+        uint256 numParticipants,
+        uint256 perParticipant
+    );
+
+    /// @dev Emitted when a participant withdraws an amount.
+    /// @param participant The address of the participant who is withdrawing.
+    /// @param amountWithdrawn The amount withdrawn.
+    /// @param percentage The percentage of the total amount withdrawn.
+    event Withdrawal(
+        address indexed participant, 
+        uint256 amountWithdrawn, 
+        uint256 percentage
+    );
+
+    /// @dev Emitted when a participant contibutes an amount.
+    /// @param participant The address of the participant who is contributing.
+    /// @param contributionAmount The amount contributed.
+    event ContributionMade(
+        address indexed participant, 
+        uint256 contributionAmount        
+    );
+
+    /// @dev Registers a participant for check splitting.
+    /// @param participant The address of the participant who is registering.
+    function registerParticipant(address participant) external;
+
+    /// @dev Initializes the bill to be divided.
+    /// @param amount The total amount of the bill.    
+    function initializeBill(uint256 amount) external;
+
+    /// @dev Allows a participant to withdraw their share.
+    function withdraw(uint256 amount) external; 
+
+    /// @dev Allows a participant to contribute to the total.
+    function contribute(uint256 amount) external; 
+
+    /// @dev Allows a participant to see how much they have left to withdraw from their share.
+    function withdrawalAmountRemaining() external view returns (uint256 amount);
+
+};
